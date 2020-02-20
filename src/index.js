d6d7505c44a588e52215d4e501b6f9a741b09152
IL1ne <IL1ne@yandex.ru>
2020-02-21 01:00:36 +0300

    feat: resolve task. all test passing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
+  if (Array.isArray(array) && array.length) {
+    minElement = array[0];
+    array.forEach(function (element) {
+      if (element < minElement) {minElement = element}
+    });
+    return minElement;      
+  }
   return 0;
 }
 
 exports.max = function max (array) {
+  if (Array.isArray(array) && array.length) {
+    maxElement = array[0];
+    array.forEach(function (element) {
+      if (element > maxElement) {maxElement = element}
+    });
+    return maxElement;
+    }
   return 0;
 }
 
 exports.avg = function avg (array) {
+  if (Array.isArray(array) && array.length) {
+    const reducer = (accumulator, currentValue) => (accumulator + currentValue)
+    return array.reduce(reducer) / array.length;
+    }
   return 0;
 }
